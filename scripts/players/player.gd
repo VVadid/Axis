cfe5d84b9233c4e5eda4a26c4459fa416f413b76
Derin Aşır <derinasir@gmail.com>
2025-11-17 19:40:17 +0300

    Make evade move break objects

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -62,6 +62,8 @@ const CAPSULE_POSITION_Y: float = 1.0
 @onready var hurtbox: Hurtbox = $Hurtbox
 @onready var hitbox: Hitbox = $Visuals/castle_guard_rig/Armature/Skeleton3D/SwordAttachment/Hitbox
 @onready var feet_hitbox: Hitbox = $FeetHitbox
+@onready var evade_hitbox: Hitbox = $EvadeHitbox
+
 
 
 func _ready() -> void:
