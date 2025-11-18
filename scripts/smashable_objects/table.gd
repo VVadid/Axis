854d571c629b9008b35eefb6083786ac79513f96
Derin Aşır <derinasir@gmail.com>
2025-11-18 23:19:45 +0300

    Add raycast under table to wake physics calculations

--- scripts/smashable_objects/table.gd
+++ scripts/smashable_objects/table.gd
@@ -3,6 +3,12 @@ extends RigidBody3D
 
 var explodes_on_breaking: bool = false
 
+
+func _physics_process(_delta: float) -> void:
+	if not $RayCast3D.is_colliding():
+		sleeping = false
+
+
 func _on_combat_manager_died(_damage_data: DamageData) -> void:
 	$ModelReplacer.replace_model(explodes_on_breaking, 6.0)
 	
