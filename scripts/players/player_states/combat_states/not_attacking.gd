705b1300202844946d622d1ef59bfb6ae5d46ccb
Derin Aşır <derinasir@gmail.com>
2025-11-20 21:50:43 +0300

    Fix a bug where player attack hitbox doesn't touch enemy hurtbox while strafing

--- scripts/players/player_states/combat_states/not_attacking.gd
+++ scripts/players/player_states/combat_states/not_attacking.gd
@@ -4,6 +4,9 @@ extends PlayerState
 @export_node_path("State") var attack1_state_path
 @onready var attack1_state = get_node(attack1_state_path)
 
+@export_node_path("State") var attack2_state_path
+@onready var attack2_state = get_node(attack2_state_path)
+
 @export_node_path("State") var heavy_attack1_state_path
 @onready var heavy_attack1_state = get_node(heavy_attack1_state_path)
 
@@ -20,7 +23,12 @@ func input(event: InputEvent) -> State:
 	super(event)
 	
 	if event.is_action_pressed("Attack") and player.combat_manager.has_enough_stamina(attack1_state.stamina_cost):
-		return attack1_state
+		if player.is_target_locked and player.input_dir.x != 0:
+			return attack2_state
+		else:
+			return attack1_state
+	
+	
 	
 	if event.is_action_pressed("Heavy Attack") and player.combat_manager.has_enough_stamina(heavy_attack1_state.stamina_cost):
 		if player.is_on_floor() and not player.is_sprinting:
